7cb5d7e3fd4174a6eba82c61a25722d8faf29e37
Nolan Docherty <sodinokazaki@gmail.com>
2021-02-01 21:09:52 +0000

    add constents file

---
+++ src/app/core/constants/constants-a.ts
@@ -0,0 +1,43 @@
+export const METRICS = [
+  {
+    text: 'Maximum Temperature',
+    value: 'Tmax'
+  }, {
+    text: 'Minimum Temperature',
+    value: 'Tmin'
+  }, {
+    text: 'Rainfall',
+    value: 'Rainfall'
+  },
+];
+
+export const AREAS = [
+  {
+    text: 'England',
+    value: 'England'
+  }, {
+    text: 'Scotland',
+    value: 'Scotland'
+  }, {
+    text: 'Wales',
+    value: 'Wales'
+  }, {
+    text: 'UK',
+    value: 'UK'
+  }
+];
+
+export const MONTHS = {
+  1: 'January',
+  2: 'Febuary',
+  3: 'March',
+  4: 'April',
+  5: 'May',
+  6: 'June',
+  7: 'July',
+  8: 'August',
+  9: 'September',
+  10: 'October',
+  11: 'November',
+  12: 'December',
+};
