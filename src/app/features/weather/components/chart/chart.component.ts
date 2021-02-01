e21d7e31aa7ca97447680ab02ec0deccba8576dc
Nolan Docherty <sodinokazaki@gmail.com>
2021-02-01 21:04:28 +0000

    add chart component

---
+++ src/app/features/weather/components/chart/chart.component.ts
@@ -0,0 +1,55 @@
+import {Component, Input, OnInit} from '@angular/core';
+import {DataPoint, WeatherChartItems} from '../../../../core/models/weather.model';
+import {Observable} from 'rxjs';
+
+@Component({
+  selector: 'app-chart',
+  templateUrl: './chart.component.html',
+  styleUrls: ['./chart.component.css']
+})
+export class ChartComponent implements OnInit{
+
+  @Input() chartData: Observable<WeatherChartItems>;
+  @Input() yAxisLabel: string;
+  @Input() xAxisLabel: string;
+
+  multi: any[];
+  view: any[];
+
+  // options
+  legend = true;
+  showLabels = true;
+  animations = true;
+  xAxis = true;
+  yAxis = true;
+  showYAxisLabel = true;
+  showXAxisLabel = true;
+  timeline = true;
+
+  colorScheme = {
+    domain: ['#5AA454', '#E44D25', '#CFC0BB', '#7aa3e5', '#a8385d', '#aae3f5']
+  };
+
+  constructor() {
+  }
+
+
+  ngOnInit(): void {
+
+    this.chartData.subscribe((data) => {
+      Object.assign(this, data);
+    });
+  }
+
+  onSelect(data): void {
+    console.log('Item clicked', JSON.parse(JSON.stringify(data)));
+  }
+
+  onActivate(data): void {
+    console.log('Activate', JSON.parse(JSON.stringify(data)));
+  }
+
+  onDeactivate(data): void {
+    console.log('Deactivate', JSON.parse(JSON.stringify(data)));
+  }
+}
